d4dcf3011eb08b1d15246cfb8ee05635bc6431e9
Julia Litvonova <julia.lit1978@gmail.com>
2022-09-22 09:57:56 +0300

    Added shape class

--- src/main/java/homework6/Main.java
+++ src/main/java/homework6/Main.java
@@ -2,7 +2,8 @@ package homework6;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        Shape shape = new Shape();
+        shape.printSomething();
 
     }
 
---
+++ src/main/java/homework6/Shape.java
@@ -0,0 +1,7 @@
+package homework6;
+
+public class Shape {
+    public void printSomething(){
+        System.out.println("Print shape");
+    }
+}
